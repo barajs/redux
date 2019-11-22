512fb914276e1cca583bf141cfd0578e4bb8b05b
nampdn <nampdn@protonmail.com>
2019-11-22 16:43:45 +0700

    fix(formula): select not correctly build

--- src/formula/select.ts
+++ src/formula/select.ts
@@ -3,6 +3,9 @@ import { Formula } from '@barajs/formula'
  * Simply select changing data.
  * @param selector
  */
-export const select = (selector: Formula) => (state: any, ...rest: any[]) => {
-  return selector(state, ...rest)
+export const select = (formula: Formula) => async (
+  state: any,
+  ...rest: any[]
+) => {
+  return await Promise.resolve(formula(state, ...rest))
 }
