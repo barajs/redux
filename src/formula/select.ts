16d64c015758d251ebae7c92cdb88f3458ce89d7
nampdn <nampdn@protonmail.com>
2019-11-22 16:32:56 +0700

    feat(formula): add ...rest params for select function

--- src/formula/select.ts
+++ src/formula/select.ts
@@ -1,7 +1,8 @@
+import { Formula } from '@barajs/formula'
 /**
  * Simply select changing data.
  * @param selector
  */
-export const select = (selector: (state: any) => any) => (state: any) => {
-  return selector(state)
+export const select = (selector: Formula) => (state: any, ...rest: any[]) => {
+  return selector(state, ...rest)
 }
