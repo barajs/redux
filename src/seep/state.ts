49406c75acc9b7fa1d2ef50b5791c4eb03ca1b4b
nampdn <nampdn@protonmail.com>
2019-11-25 20:42:02 +0700

    fix(stateProp): debugging

--- src/seep/state.ts
+++ src/seep/state.ts
@@ -10,6 +10,7 @@ export interface StatePropSelector {
   selector: (state: any) => any
 }
 
+const lastState: any = {}
 export const stateProp = (selectorData: StatePropSelector) => (
   _: any,
   contextes: any,
@@ -17,14 +18,20 @@ export const stateProp = (selectorData: StatePropSelector) => (
   const data: any = getContext(BARA_REDUX, contextes)
   const store: Store = data.store
   const initialState = data.initialState
-  const lastState = data.lastState
+  //   const lastState = data.lastState
 
   const { prop, selector } = selectorData
   const current = selector(store.getState())
   const last =
     lastState[prop] === undefined ? selector(initialState) : lastState[prop]
 
-  if (!isEqual(current, last)) {
+  const shouldPass = !isEqual(current, last)
+  console.log(
+    `Should pass: ${shouldPass}`,
+    JSON.stringify(last),
+    JSON.stringify(current),
+  )
+  if (shouldPass) {
     lastState[prop] = current
     return true
   }
