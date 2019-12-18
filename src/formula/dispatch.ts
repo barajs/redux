627495fab09be432cefe268d2d4faa99f9087e28
nampdn <nampdn@protonmail.com>
2019-12-18 19:58:43 +0700

    feat(formula): add dispatchInvoke function

--- src/formula/dispatch.ts
+++ src/formula/dispatch.ts
@@ -1,6 +1,6 @@
 import { Action, Store } from 'redux'
 import { withStore } from './withStore'
-import { Formula } from '@barajs/formula'
+import { Formula, invoke } from '@barajs/formula'
 
 /**
  * Dispatch a Redux action to store to change some state.
@@ -10,5 +10,15 @@ export const dispatch = withStore(
     const action: Action = await Promise.resolve(formula(payload, ...rest))
     store.dispatch(action)
     return payload
+  },
+)
+
+export const dispatchInvoke = withStore(
+  (dispatcher: string, formula: Formula) => async (
+    payload: any,
+    store: Store,
+    ...rest: any[]
+  ) => {
+    return await invoke(dispatcher, formula)(payload, store.dispatch, ...rest)
   },
 )
