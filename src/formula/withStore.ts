9e7961e7d16166cb7cb688858c40456cd1e6c48d
nampdn <nampdn@protonmail.com>
2019-12-19 09:41:15 +0700

    fix(formula): withStore can take multiple arguments

--- src/formula/withStore.ts
+++ src/formula/withStore.ts
@@ -5,12 +5,12 @@ import { Store } from 'redux'
 import { BARA_REDUX, ReduxFormula } from '../types'
 
 export const withStore = (formula: Formula | ReduxFormula) => (
-  options?: any,
+  ...options: any[]
 ) => async (payload: any, contextes: any) => {
   const data: any = getContext(BARA_REDUX, contextes)
   const store: Store = data.store
   const next = await Promise.resolve(
-    formula(options)(payload, store, contextes),
+    formula(...options)(payload, store, contextes),
   )
   return next
 }
